574962ee3ece28f716359d416b1e2bd35f62bc7a
Riccardo Torre <riccardo.torre@studenti.univr.it>
2023-12-27 15:31:55 +0100

    skeleton
    contains just the project skeleton

---
+++ src/main/java/org/iLearn/iLearnApp/ILearnAppApplication.java
@@ -0,0 +1,13 @@
+package org.iLearn.iLearnApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILearnAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILearnAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iLearn/iLearnApp/ILearnAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iLearn.iLearnApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILearnAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
