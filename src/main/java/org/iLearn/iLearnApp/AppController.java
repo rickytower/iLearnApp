a20ec4cd0ddbde987094cd79c423fc8857da34b2
Toffa97 <andrea@toffaletti.it>
2024-01-12 17:12:14 +0100

    1) AppController update to have exam + profile html interface 2) several html file created 3) created .gitignore file
    TODO: populate the html file

--- src/main/java/org/iLearn/iLearnApp/AppController.java
+++ src/main/java/org/iLearn/iLearnApp/AppController.java
@@ -2,11 +2,33 @@ package org.iLearn.iLearnApp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class AppController {
     @RequestMapping("/")
     public String index(){
         return "login";
+    }
+
+    @RequestMapping("/profile")
+    public String profile(){
+        return "profile";
+    }
+
+    @RequestMapping("/exam")
+    public String exam(){
+        return "exam";
+    }
+
+    @RequestMapping("/exam-list")
+    public String examList(){
+        return "exam-list";
+    }
+
+    @RequestMapping("/exam-book")
+    public String examBOok(){
+        return "exam-book";
     }
 
 }
