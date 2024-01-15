79684bd848228743350f91d3258af5b6bb85e15f
Toffa97 <andrea@toffaletti.it>
2024-01-15 16:08:23 +0100

    Added profile_edit.html + update profile.html
    Added test for login and role
    Added test to check the completeness of intiDB for the user entities

--- src/test/java/org/iLearn/iLearnApp/ILearnAppApplicationTests.java
+++ src/test/java/org/iLearn/iLearnApp/ILearnAppApplicationTests.java
@@ -1,9 +1,11 @@
 package org.iLearn.iLearnApp;
 
 import io.restassured.RestAssured;
+import org.iLearn.iLearnApp.model.entity.RoleType;
 import org.iLearn.iLearnApp.model.entity.UserRegistred;
 import org.iLearn.iLearnApp.model.repository.UserRegistredRepository;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
@@ -13,9 +15,10 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -27,16 +30,47 @@ class ILearnAppApplicationTests {
 	@Autowired
 	private UserRegistredRepository userRegistredRepository;
 
-	@BeforeAll
+	@BeforeEach
 	void setUp() throws IOException {
 		RestAssured.baseURI = "http://localhost:8080";
 		utils.initDB();
 	}
 
+	/**
+	 * Check if all the users in the json are correctly insert in the db
+	 * IN particular, they shall be 5.
+	 */
 	@Test
 	@DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
-	public void login() {
-		Optional<UserRegistred> userRegistredList = userRegistredRepository.findByUsernameAndPassword("john.doe", "password123");
+	public void initDBTest() {
+		Iterable<UserRegistred> userRegistredList = userRegistredRepository.findAll();
+		assertEquals(((List<UserRegistred>) userRegistredList).size(),5);
+	}
+
+	/**
+	 * Login attempt with PROFESSOR ENTITY
+	 * Test to check if the role after the login attempt IS NOT STUDENT
+	 *
+	 * (in particular, from the StrundetsData.json search PROFESSOR)
+	 */
+	@Test
+	@DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+	public void loginAsProfessor() {
+		Optional<UserRegistred> userRegistredList = userRegistredRepository.findByUsernameAndPassword("mariano", "ceccato");
+		assertEquals(userRegistredList.get().getRoleType(), RoleType.PROFESSOR);
+	}
+
+	/**
+	 * Login attempt with STUDENT ENTITY
+	 *
+	 */
+	@Test
+	@DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+	public void loginAsStudent() {
+		Optional<UserRegistred> userRegistredList = userRegistredRepository.findByUsernameAndPassword("jane.smith", "password456");
 		assertFalse(userRegistredList.isEmpty());
 	}
+
+
+
 }
